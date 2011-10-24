e6b63e85f5f194db692918175899316ea56801c1
Lan Ta <lan.ta@c-mg.com>
2011-10-24 02:59:26 +0000

    upload screen

---
+++ MonitoringGae/src/main/java/cmg/org/monitor/module/Dashboard/client/Dashboard.java
@@ -0,0 +1,13 @@
+package cmg.org.monitor.module.Dashboard.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class Dashboard implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ MonitoringGae/src/main/java/cmg/org/monitor/module/SystemDetail/client/SystemDetail.java
@@ -0,0 +1,13 @@
+package cmg.org.monitor.module.SystemDetail.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class SystemDetail implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
