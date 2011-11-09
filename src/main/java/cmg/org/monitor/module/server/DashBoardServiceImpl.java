6ef054c903c12dd752e50dc944beee9e61851101
Hai Lu <hai.lu@c-mg.com>
2011-11-09 03:24:30 +0000

    modified some methods

--- src/main/java/cmg/org/monitor/module/server/DashBoardServiceImpl.java
+++ src/main/java/cmg/org/monitor/module/server/DashBoardServiceImpl.java
@@ -1,9 +1,13 @@
 package cmg.org.monitor.module.server;
 
+import java.util.logging.Logger;
+
 import cmg.org.monitor.dao.SystemMonitorDAO;
 import cmg.org.monitor.dao.impl.SystemMonitorDaoJDOImpl;
 import cmg.org.monitor.entity.shared.SystemMonitor;
 import cmg.org.monitor.module.client.DashBoardService;
+import cmg.org.monitor.services.MonitorService;
+
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 public class DashBoardServiceImpl extends RemoteServiceServlet implements DashBoardService {
@@ -12,6 +16,9 @@ public class DashBoardServiceImpl extends RemoteServiceServlet implements DashBo
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	private static final Logger logger = Logger.getLogger(MonitorService.class
+			.getCanonicalName());
 
 	@Override
 	public SystemMonitor[] listSystems() {	
--- src/main/java/cmg/org/monitor/module/server/SystemDetailServiceImpl.java
+++ src/main/java/cmg/org/monitor/module/server/SystemDetailServiceImpl.java
@@ -1,5 +1,7 @@
 package cmg.org.monitor.module.server;
 
+import java.util.logging.Logger;
+
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 import cmg.org.monitor.dao.CpuMemoryDAO;
@@ -10,8 +12,10 @@ import cmg.org.monitor.dao.impl.CpuMemoryDaoJDOImpl;
 import cmg.org.monitor.dao.impl.FileSystemDaoJDOImpl;
 import cmg.org.monitor.dao.impl.ServiceMonitorDaoJDOImpl;
 import cmg.org.monitor.dao.impl.SystemMonitorDaoJDOImpl;
+import cmg.org.monitor.entity.shared.CpuMemory;
 import cmg.org.monitor.entity.shared.SystemMonitor;
 import cmg.org.monitor.module.client.SystemDetailService;
+import cmg.org.monitor.services.MonitorService;
 
 public class SystemDetailServiceImpl extends RemoteServiceServlet implements SystemDetailService {
 
@@ -24,6 +28,9 @@ public class SystemDetailServiceImpl extends RemoteServiceServlet implements Sys
 	CpuMemoryDAO cmDAO = new CpuMemoryDaoJDOImpl();
 	FileSystemDAO fsDAO = new FileSystemDaoJDOImpl();
 	ServiceMonitorDAO smDAO = new ServiceMonitorDaoJDOImpl();
+	
+	private static final Logger logger = Logger.getLogger(SystemDetailServiceImpl.class
+			.getCanonicalName());
 	
 	public SystemMonitor getLastestDataMonitor(String sysID) {		
 		SystemMonitor sys = null;
@@ -40,6 +47,7 @@ public class SystemDetailServiceImpl extends RemoteServiceServlet implements Sys
 			}
 		} catch (Exception ex) {
 			ex.printStackTrace();
+			logger.info("Cannot get data from database\n" + ex.getMessage());
 		}		
 		return sys;
 	}
@@ -51,8 +59,24 @@ public class SystemDetailServiceImpl extends RemoteServiceServlet implements Sys
 			b = sysDAO.getSystembyID(sysID) != null;
 		} catch (Exception ex) {
 			b= false;
+			ex.printStackTrace();
+			logger.info("Invalid System ID or Error when load from database.\n" + ex.getMessage());
 		}
 		return b;
+	}
+
+	@Override
+	public CpuMemory[] listCpuMemoryHistory(String sysID) {
+		CpuMemory[] list = null;
+		SystemMonitor sys = null;
+		try {
+			sys = sysDAO.getSystembyID(sysID);
+			list = cmDAO.getLastestCpuMemory(sys, 64800);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			logger.info("Cannot get data from database\n" + ex.getMessage());
+		}
+		return list;
 	}
 
 }
