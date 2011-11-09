34df3f01abc985338a99126b2e7fe604122f5216
Lan Ta <lan.ta@c-mg.com>
2011-11-09 07:39:30 +0000

    Dto

---
+++ MonitoringGae/src/main/java/cmg/org/monitor/ext/model/shared/MonitorEditDto.java
@@ -0,0 +1,88 @@
+package cmg.org.monitor.ext.model.shared;
+
+import java.io.Serializable;
+import java.util.List;
+
+@SuppressWarnings("serial")
+public class MonitorEditDto implements Serializable{
+	String id;
+	
+	String name;
+	
+	String group;
+	
+	String[] groups;
+	
+	boolean isActive;
+	
+	String protocol;
+	
+	String ip;
+	
+	String url;
+	
+	String remoteURl;
+	
+	public String getRemoteURl() {
+		return remoteURl;
+	}
+	public void setRemoteURl(String remoteURl) {
+		this.remoteURl = remoteURl;
+	}
+	int select;
+	public int getSelect() {
+		return select;
+	}
+	public void setSelect(int select) {
+		this.select = select;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGroup() {
+		return group;
+	}
+	public void setGroup(String group) {
+		this.group = group;
+	}
+	public String[] getGroups() {
+		return groups;
+	}
+	public void setGroups(String[] groups) {
+		this.groups = groups;
+	}
+	public boolean isActive() {
+		return isActive;
+	}
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+	public String getProtocol() {
+		return protocol;
+	}
+	public void setProtocol(String protocol) {
+		this.protocol = protocol;
+	}
+	public String getIp() {
+		return ip;
+	}
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+}
