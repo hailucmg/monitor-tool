33df17bbd98fa0b525c8cbdbcb9bbe7cc8892cf3
Lan Ta <lan.ta@c-mg.com>
2011-11-09 07:39:30 +0000

    Dto

---
+++ src/main/java/cmg/org/monitor/ext/model/shared/UserDto.java
@@ -0,0 +1,42 @@
+package cmg.org.monitor.ext.model.shared;
+
+import java.io.Serializable;
+
+public class UserDto implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	String username;
+	String group;
+	String email;
+	
+	public String getGroup() {
+		return group;
+	}
+	public void setGroup(String group) {
+		this.group = group;
+	}
+	
+	public UserDto(){}
+	public UserDto(String username, String email, String group, String permission){
+		this.username = username;
+		this.email = email;
+		this.group = group;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
