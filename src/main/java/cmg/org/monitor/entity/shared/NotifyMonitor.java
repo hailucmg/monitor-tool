d33738c897310b1e411089646847e158907387f5
Lan Ta <lan.ta@c-mg.com>
2012-01-06 06:53:59 +0000

    CIP-201:create object

---
+++ src/main/java/cmg/org/monitor/entity/shared/NotifyMonitor.java
@@ -0,0 +1,98 @@
+package cmg.org.monitor.entity.shared;
+
+import java.io.Serializable;
+
+import javax.jdo.annotations.Extension;
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+@PersistenceCapable(identityType = IdentityType.APPLICATION, detachable = "true")
+public class NotifyMonitor implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@PrimaryKey
+	@Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+	@Extension(vendorName = "datanucleus", key = "gae.encoded-pk", value = "true")
+	private String id;
+
+	@Persistent
+	private String sid;
+
+	@Persistent
+	private boolean isNotifyCpu;
+
+	@Persistent
+	private boolean isNotifyMemory;
+
+	
+	@Persistent
+	private boolean isNotifyServices;
+
+	@Persistent
+	private boolean isNotifyServicesConnection;
+
+	public NotifyMonitor() {
+	}
+
+	public NotifyMonitor(String sid, boolean isNotifyCpu,
+			boolean isNotifyMemory,
+			boolean isNotifyServices, boolean isNotifyServicesConnection) {
+		this.sid = sid;
+		this.isNotifyCpu = isNotifyCpu;
+		this.isNotifyMemory = isNotifyMemory;
+		this.isNotifyServices = isNotifyServices;
+		this.isNotifyServicesConnection = isNotifyServicesConnection;
+	}
+
+	public String getSid() {
+		return sid;
+	}
+
+	public void setSid(String sid) {
+		this.sid = sid;
+	}
+
+	public boolean isNotifyCpu() {
+		return isNotifyCpu;
+	}
+
+	public void setNotifyCpu(boolean isNotifyCpu) {
+		this.isNotifyCpu = isNotifyCpu;
+	}
+
+	public boolean isNotifyMemory() {
+		return isNotifyMemory;
+	}
+
+	public void setNotifyMemory(boolean isNotifyMemory) {
+		this.isNotifyMemory = isNotifyMemory;
+	}
+
+	public boolean isNotifyServices() {
+		return isNotifyServices;
+	}
+
+	public void setNotifyServices(boolean isNotifyServices) {
+		this.isNotifyServices = isNotifyServices;
+	}
+
+	public boolean isNotifyServicesConnection() {
+		return isNotifyServicesConnection;
+	}
+
+	public void setNotifyServicesConnection(boolean isNotifyServicesConnection) {
+		this.isNotifyServicesConnection = isNotifyServicesConnection;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+}
