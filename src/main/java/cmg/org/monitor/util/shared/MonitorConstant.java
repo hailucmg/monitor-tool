a18c4748dcca44956bc099fa3cd1a971e33eb919
Lan Ta <lan.ta@c-mg.com>
2012-01-06 06:51:53 +0000

    CIP-200:add some field

--- src/main/java/cmg/org/monitor/util/shared/MonitorConstant.java
+++ src/main/java/cmg/org/monitor/util/shared/MonitorConstant.java
@@ -1,20 +1,30 @@
 package cmg.org.monitor.util.shared;
 
 public final class MonitorConstant {
+	
+	
+	public static final String Notify_Cpu = "When system uses more than 90% CPU :";
+	
+	public static final String Notify_Memory = "When system uses more than 90% Memory :";
+	
+	public static final String Notify_Service = "When any service are down :";
+	
+	public static final String Notify_ServiceConnection = "When any service connection are too slow :";
+	
 	public static final int CPU_MEMORY_REFRESH_RATE = 1000;
 	// Alert mail sender service
 	public static final String IMAGES_FOR_EMAIL ="monitor.c-mg.vn\\images\\logo\\c-mg_logo.png" ;
 	
 	public static final String ALERTSTORE_DEFAULT_NAME = "Issue Warning";
 	
-	public static final String PROJECT_HOST_NAME = "monitor.c-mg.vn";
+	public static final String PROJECT_HOST_NAME = "monitor.c-mg.com";
 	
-	public static final String ALERT_MAIL_SENDER_NAME = "admin@cmg-app-eng.appspotmail.com";
+	public static final String ALERT_MAIL_SENDER_NAME = "admin@future-monitor.appspotmail.com";
 
 	public static final int STATISTIC_HISTORY_LENGTH = 999;
 
 	public static final int CPU_MEMORY_HISTORY_LENGTH = 100;
-
+	
 	public static String SMTP_PROTOCOL = "SMTP";
 
 	public static String HTTP_PROTOCOL = "HTTP(s)";
@@ -31,14 +41,14 @@ public final class MonitorConstant {
 
 	public static final int REDIRECT_WAIT_TIME = 5000;
 
-	public static final String DOMAIN = "c-mg.vn";
+	public static final String DOMAIN = "c-mg.com";
 
-	public static final String ADMIN_EMAIL = "monitor@c-mg.vn";
+	public static final String ADMIN_EMAIL = "monitor@c-mg.com";
 	public static final String ADMIN_EMAIL_ID = "monitor";
 
-	public static final String ADMIN_PASSWORD = "31102011";
+	public static final String ADMIN_PASSWORD = "w3lcom3back";
 	
-	public static final String ADMIN_MONITOR_GROUP = "admin";
+	public static final String ADMIN_MONITOR_GROUP = "adminmonitor";
 
 	public static final int ROLE_ADMIN = 0x001;
 	public static final int ROLE_NORMAL_USER = 0x002;
