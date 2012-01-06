ba9d711d5f52fba404e3108817b4713078641418
Hai Lu <hai.lu@c-mg.com>
2012-01-06 03:38:13 +0000

    merging ...

--- src/main/java/cmg/org/monitor/dao/JVMMemoryDAO.java
+++
@@ -1,9 +0,0 @@
-package cmg.org.monitor.dao;
-
-import cmg.org.monitor.entity.shared.JVMMemory;
-import cmg.org.monitor.entity.shared.SystemMonitor;
-
-public interface JVMMemoryDAO {
-
-	JVMMemory getLastestJvm(SystemMonitor system);
-}
