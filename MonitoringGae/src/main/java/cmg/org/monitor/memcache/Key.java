7a64ada8c60bb8567d915e442ecc8778bef9f9d2
Hai Lu <hai.lu@c-mg.com>
2012-01-13 09:57:03 +0000

    change configuration for release 2.0

--- MonitoringGae/src/main/java/cmg/org/monitor/memcache/Key.java
+++ MonitoringGae/src/main/java/cmg/org/monitor/memcache/Key.java
@@ -27,8 +27,8 @@ public class Key implements Serializable {
 	public static final int LIST_GROUP = 0x013;
 	public static final int LIST_ALL_USERS = 0x014;
 	public static final int LIST_USERS_IN_GROUP = 0x015;
-	public static final int CHANGE_LOG = 0x022;
-	public static final int CHANGE_LOG_COUNT = 0x023;
+	public static final int CHANGE_LOG = 0x016;
+	public static final int CHANGE_LOG_COUNT = 0x017;
 	
 	
 	private int type;
